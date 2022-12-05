57107d10f9825a1e04effd26bd6a030cf3df70db
Felipe Silveira <felipe-p-s2011@hotmail.com>
2022-12-05 10:49:05 -0300

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,12 @@
+﻿param($rede)
+if (!$rede)
+{
+write-host "Modo de uso ./script.ps1 192.168.1"
+}
+else{
+foreach ($ip in 1..254)  {
+try{$MeuPing = ping -n 1 "$rede.$ip" | select-string "TTL" 
+$MeuPing.Line.Split(' ')[2] -replace ":",""
+    } catch {}
+                         }
+    }
